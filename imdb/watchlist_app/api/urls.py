209a352319329cb12ecd8914b7733077323e8e9b
rafiqulislam21 <ribijoy19@gmail.com>
2022-02-06 09:19:12 +0100

    19 viewsets and routers

--- imdb/watchlist_app/api/urls.py
+++ imdb/watchlist_app/api/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 # from watchlist_app.api.views import movie_list, movie_details
 from watchlist_app.api.views import (WatchListAV, WatchDetailsAV, 
                                      StreamPlatformAV, StreamPlatformDetailsAV, 
-                                     ReviewList, ReviewDetails, ReviewCreate)
+                                     ReviewList, ReviewDetails, ReviewCreate,
+                                     StreamPlatformVS)
 
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch-list'),
     path('<int:pk>', WatchDetailsAV.as_view(), name='watch-details'),
-    path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>', StreamPlatformDetailsAV.as_view(),
-         name='stream-platform-details'),
+    path('', include(router.urls)),
+    # path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    # path('stream/<int:pk>', StreamPlatformDetailsAV.as_view(),name='stream-platform-details'),
+    
     # path('review/', ReviewList.as_view(), name='review-list'),
     # path('review/<int:pk>', ReviewDetails.as_view(), name='review-details'),
     path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
--- imdb/watchlist_app/api/views.py
+++ imdb/watchlist_app/api/views.py
@@ -1,17 +1,31 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import viewsets
 
 from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
 
 # Create your views here.
+# viewsets with routers
+class StreamPlatformVS(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = StreamPlatform.objects.all()
+        watchlist = get_object_or_404(queryset, pk=pk)
+        serializer = StreamPlatformSerializer(watchlist)
+        return Response(serializer.data)
+
 # generic without mixins
-
-
 class ReviewCreate(generics.CreateAPIView):
     # queryset = Review.objects.all()
     serializer_class = ReviewSerializer
