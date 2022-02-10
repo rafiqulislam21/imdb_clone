1fe8e557a46dd5211452dfa50a2e7d954b721faa
rafiqulislam21 <ribijoy19@gmail.com>
2022-02-10 09:30:45 +0100

    jwt auth

--- imdb/watchlist_app/api/views.py
+++ imdb/watchlist_app/api/views.py
@@ -74,7 +74,7 @@ class ReviewList(generics.ListAPIView):
     # queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     # permission_classes = [IsAuthenticatedOrReadOnly] #permission_classes
-    # permission_classes = [IsAuthenticated] #permission_classes
+    permission_classes = [IsAuthenticated] #permission_classes
 
     def get_queryset(self):
         pk = self.kwargs['pk']
