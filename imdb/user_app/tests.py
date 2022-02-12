47e2204afa878ddf83ddf04d5e04f425a82ba15b
rafiqulislam21 <ribijoy19@gmail.com>
2022-02-13 00:07:12 +0100

    test cases

--- imdb/user_app/tests.py
+++ imdb/user_app/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+class RegisterTestCase(APITestCase):
+    def test_register(self):
+        data = {
+            'username': 'tasecase',
+            'email': 'testcase@example.com',
+            'password': 'NewPassword@123',
+            'password2': 'NewPassword@123',
+        }
+        
+        url = reverse('register')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # self.assertEqual(Account.objects.count(), 1)
+        # self.assertEqual(Account.objects.get().name, 'DabApps')
